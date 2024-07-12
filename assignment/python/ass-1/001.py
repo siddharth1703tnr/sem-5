4cfaa3112c8addcfac033e08df3bf5d1dbee7d60
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-07-12 08:30:26 +0530

    Create 001.py

---
+++ assignment/python/ass-1/001.py
@@ -0,0 +1,19 @@
+# assign-1 Program-1
+# 1. Calculate Addition,Subtraction,Multiplication and Division from 2 numbers provided by user input
+num1 = int(input("Enter Number :- "))
+num2 = int(input("Enter Number :- "))
+add = num1+num2
+sub = num1-num2
+mul = num1*num2
+div = num1/num2
+print(type(div))
+print(add, sub, mul, div)
+
+
+# OUTPUT =====>
+#Enter Number :- 100
+#Enter Number :- 99
+#<class 'float'>
+#199 1 9900 1.0101010101010102
+#
+#=== Code Execution Successful ===
