8b9e2189d41d21b61b4859c103dcac5fccf0b58d
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-07-12 08:59:06 +0530

    Update 003.py
    code added

--- assignment/python/ass-1/003.py
+++ assignment/python/ass-1/003.py
@@ -1,5 +1,31 @@
 # assign-1 Program-2
 # 3. Create a Marksheet for 5 subjects and calculate total, average and grade with if else.
 
+html = int(input("Enter Mark:-"))
+css = int(input("Enter Mark:-"))
+js = int(input("Enter Mark:-"))
+php = int(input("Enter Mark:-"))
+python = int(input("Enter Mark:-"))
+
+total = (html+css+js+php+python)
+avg = total/5
+
+if ((avg>90) and (avg<100)) {
+    print("0")
+}else((avg>80) and (avg<89)) {
+    print("A")
+}else((avg>70) and (avg<79)) {
+    print("B")
+}else((avg>60) and (avg<69)) {
+    print("C")
+}else((avg>50) and (avg<59)) {
+    print("D")
+}else((avg>40) and (avg<49)) {
+    print("E")
+}else((avg>39) and (avg<1)) {
+    print("F")
+}
+
+
 
 # OUTPUT ===>
