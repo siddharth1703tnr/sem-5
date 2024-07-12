b023d32006e0a7ab1769bc9524d18af8378a2255
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-07-12 09:12:46 +0530

    Update 003.py
    v0.0.2

--- assignment/python/ass-1/003.py
+++ assignment/python/ass-1/003.py
@@ -1,41 +1,30 @@
 # assign-1 Program-2
 # 3. Create a Marksheet for 5 subjects and calculate total, average and grade with if else.
 
-html = int(input("Enter Mark:-"))
-css = int(input("Enter Mark:-"))
-js = int(input("Enter Mark:-"))
-php = int(input("Enter Mark:-"))
-python = int(input("Enter Mark:-"))
+html = int(input("HTML Mark:-"))
+css = int(input("CSS Mark:-"))
+js = int(input("JS Mark:-"))
+php = int(input("PHP Mark:-"))
+python = int(input("PYTHON Mark:-"))
 
 total = (html+css+js+php+python)
 avg = total/5
 
 if ((avg>90) and (avg<100)):
-    {
     print("0")
-    }
-    elif((avg>80) and (avg<89)):
-    {
+elif ((avg>80) and (avg<89)):
     print("A")
-    }
-    elif((avg>70) and (avg<79)):
-    {
+elif ((avg>70) and (avg<79)):
     print("B")
-    }
-    elif((avg>60) and (avg<69)):
-    {
+elif ((avg>60) and (avg<69)):
     print("C")
-    }
-    elif((avg>50) and (avg<59)):
-    {
+elif ((avg>50) and (avg<59)):
     print("D")
-    }elif((avg>40) and (avg<49)):
-    {
+elif ((avg>40) and (avg<49)):
     print("E")
-    }else:
-    {
+else:
     print("F")
-    }
+    
 
 
 # OUTPUT ===>
