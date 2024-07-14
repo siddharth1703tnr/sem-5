1f28a46ab3915d39016884c92251d21606d897a3
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-07-14 20:09:37 +0530

    Create 005.py

---
+++ assignment/python/ass-1/005.py
@@ -0,0 +1,32 @@
+# assign-1 Program-5
+# 5. Print 1 to 10 and 10 to 1 with for loop.
+
+
+for i in range (1, 10):
+    print(i)
+for i in range (10, 0, -1):
+    print(i)
+
+  
+# OUTPUT ===>
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+#9
+#8
+#7
+#6
+#5
+#4
+#3
+#2
+#1
+#
+# === Code Execution Successful ===
