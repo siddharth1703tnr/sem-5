d34552946eace56586cf4ca114bc42e5bbd638f9
siddharth1703tnr <bca2022siddharth1703@tnraocollege.org>
2024-07-14 20:03:40 +0530

    004.py
    full code done

--- assignment/python/ass-1/004.py
+++ assignment/python/ass-1/004.py
@@ -1,4 +1,4 @@
-# assign-1 Program-2
+# assign-1 Program-4
 # 4. Write a program to add employee names in a list EMPNAME and perform add,remove and append methods.
 
 
@@ -9,7 +9,22 @@ print(EMPNAME)
 print(len(EMPNAME))
 print(type(EMPNAME))
 
+EMPNAME.append("ramesh")
+print(EMPNAME)
 
+EMPNAME.remove("shyam")
+print(EMPNAME)
+
+EMPNAME.insert(2, "shyam")
+print(EMPNAME)
 
 
 # OUTPUT ===>
+# ['ram', 'shyam', 'het']
+# 3
+# <class 'list'>
+# ['ram', 'shyam', 'het', 'ramesh']
+# ['ram', 'het', 'ramesh']
+# ['ram', 'het', 'shyam', 'ramesh']
+
+# === Code Execution Successful ===
